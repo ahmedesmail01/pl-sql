10e452be66f113baf8800f71492f675c087b46fe
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-08-28 16:08:42 +0300

    refactor: update hire_date format and add aggregate functions for employee data

--- khudary-sql.sql
+++ khudary-sql.sql
@@ -31,7 +31,27 @@ select first_name, round(sysdate - hire_date) / 365 as duration  from employees;
 select first_name, next_day(hire_date, 'MONDAY') as duration from employees;
 select first_name, next_day(hire_date, 2) as duration from employees;
 --date full date format
-select hire_date, to_char(hire_date, 'DD-MON-YYYY') as duration from employees;
+select hire_date, to_char(hire_date, 'DD-MON-YYYY hh:mi:ss pm') as duration from employees;
 select hire_date, add_months(hire_date, 6) as duration from employees;
+
+
+select to_char(1477, '9,999') v from dual;
+select to_char(1477, '$09999') v from dual;
+
+
+select to_number('1,477', '9,999') v from dual;
+
+-- get date as dd/mm/yyyy  => 05/09/1998 format
+select to_date('05-09-1998', 'dd/mm/YYYY') v from dual;
+
+
+select first_name, hire_date, sum(salary) from employees group by first_name, hire_date;
+select department_id, sum(salary) from employees group by department_id;
+
+select first_name, salary from employees where salary = (select max(salary) from employees);
+
+
+select listagg(first_name, ', ') within group (order by first_name) as names from employees;
+
 
   
