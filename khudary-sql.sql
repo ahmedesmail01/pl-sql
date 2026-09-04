0620734281db77749db5c588ace16478a8e08b2f
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-09-04 19:08:09 +0300

    refactor: add department-related queries and create backup tables for employees and departments

--- khudary-sql.sql
+++ khudary-sql.sql
@@ -53,5 +53,50 @@ select first_name, salary from employees where salary = (select max(salary) from
 
 select listagg(first_name, ', ') within group (order by first_name) as names from employees;
 
+select employees.first_name , departments.department_name from employees , departments where 
+employees.department_id = departments.department_id;
 
-  
+desc departments;
+
+
+create table copy_dept as select * from departments;
+commit;
+
+select * from copy_dept;
+
+savepoint before_delete;
+delete from copy_dept where department_id = 10;
+rollback to before_delete;
+
+select  departments from user_tables;
+
+select * from employees where department_id = 20;
+
+create table marketing_employees as select * from employees where department_id = 20;
+
+select * from marketing_employees;
+
+
+desc employees;
+
+create table employees_copy as select * from employees;
+select * from employees_copy;
+insert into employees_copy select * from employees where department_id = 30;
+commit;
+
+alter table employees_copy add (gender varchar2(10) default 'male');
+commit;
+
+desc employees_copy;
+
+-- alter table employees_copy modify (gender varchar2(10) default "male");
+alter table employees_copy drop (gender);
+desc employees_copy;
+
+rollback;
+
+select * from employees_copy;  
+
+select email , to_date(hire_date, 'dd-mm-yyyy') as hire_date from employees_copy where hire_date > to_date('05-09-2016', 'dd-mm-yyyy') order by hire_date;
+
+rename employees_copy to employees_backup;
