0842b4a7c13167b36d704528e401699a52c3bbd5
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-09-24 14:09:00 +0300

    feat: add logic to retrieve and display employee name by ID with error handling

--- Khudary-pl-sql.sql
+++ Khudary-pl-sql.sql
@@ -1,23 +1,20 @@
 begin
-dbms_output.put_line('hello pl sql');
-dbms_output.put_line('father''s day');
-dbms_output.put_line(q'(father's day)');
+   dbms_output.put_line('hello pl sql');
+   dbms_output.put_line('father''s day');
+   dbms_output.put_line(q'(father's day)');
 end;
 /
 
-                  
-declare 
 
-v_name varchar(20) := 'Ahmed';
-v_age number := 28;
-v_date date;
-
-
+declare
+   v_name varchar(20) := 'Ahmed';
+   v_age  number := 28;
+   v_date date;
 begin
-dbms_output.put_line('Name: ' || v_name);
-dbms_output.put_line('Age: ' || v_age);
-v_date := '2-sep-2026';
-dbms_output.put_line('Date: ' || v_date);
+   dbms_output.put_line('Name: ' || v_name);
+   dbms_output.put_line('Age: ' || v_age);
+   v_date := '2-sep-2026';
+   dbms_output.put_line('Date: ' || v_date);
 end;
 /
 
@@ -25,10 +22,11 @@ declare
  
 --  variable v_sal employees.salary%type;
 
-
-begin
-
-select salary into :v_sal from employees where employee_id = 100;
+ begin
+   select salary
+     into :v_sal
+     from employees
+    where employee_id = 100;
 -- dbms_output.put_line('Salary: ' || :v_sal);
 end;
 /
@@ -36,63 +34,62 @@ end;
 print v_sal;
 
 
-select *  from user_tables where table_name like '%MPLOY%';
+select *
+  from user_tables
+ where table_name like '%MPLOY%';
 
 
-select * from EMPLOYEES  where employee_id = 100;
+select *
+  from employees
+ where employee_id = 100;
 
 
-select * from employees where department_id = 20;
+select *
+  from employees
+ where department_id = 20;
 
 
-declare 
+declare
+   v_rows_updated number := 0;
+begin
+   update employees
+      set
+      salary = salary + 200
+    where department_id = 20;
 
-v_rows_updated number := 0 ;
-
-begin 
-
-update employees set salary = salary + 200 where department_id = 20;
-
-v_rows_updated := sql%rowcount;
-
-dbms_output.put_line('Rows Updated' || v_rows_updated);
+   v_rows_updated := sql%rowcount;
+   dbms_output.put_line('Rows Updated' || v_rows_updated);
 end;
 /
 
 
 declare
-
-v_num number := &v;
-
-begin 
-
-if v_num > 10 then 
-dbms_output.put_line('Number is greater than 10');
-else
-dbms_output.put_line('Number is less than or equal to 10');
-end if;
-
+   v_num number := &v;
+begin
+   if v_num > 10 then
+      dbms_output.put_line('Number is greater than 10');
+   else
+      dbms_output.put_line('Number is less than or equal to 10');
+   end if;
 end;
 /
 
 
 -- collections
 
-
-declare 
-
-type tab_no is table of varchar2(100) 
-index by pls_integer;
-
-v_tab tab_no;
-
-begin 
-
-v_tab(1) := 'Ahmed';
-v_tab(2) := 'Esmail';   
-v_tab(5) := 'Nasr';
-
-dbms_output.put_line('my full name is : ' || v_tab(1) || ' ' || v_tab(5) || ' ' || v_tab(2));
+declare
+   type tab_no is
+      table of varchar2(100) index by pls_integer;
+   v_tab tab_no;
+begin
+   v_tab(1) := 'Ahmed';
+   v_tab(2) := 'Esmail';
+   v_tab(5) := 'Nasr';
+   dbms_output.put_line('my full name is : '
+                        || v_tab(1)
+                        || ' '
+                        || v_tab(5)
+                        || ' ' || v_tab(2));
 
 
 end;
@@ -101,29 +98,36 @@ end;
 
 -- nested table
 
-
 declare
-type loc_type is table of varchar2(200);
+   type loc_type is
+      table of varchar2(200);
+   v_loc loc_type := loc_type(
+      'cairo',
+      'alex',
+      'giza'
+   );
+begin
+   dbms_output.put_line('my locations are : '
+                        || v_loc(1)
+                        || ' '
+                        || v_loc(2)
+                        || ' ' || v_loc(3));
 
-v_loc loc_type := loc_type('cairo', 'alex', 'giza');
-begin 
 
-dbms_output.put_line('my locations are : ' || v_loc(1) || ' ' || v_loc(2) || ' ' || v_loc(3));
-
-
-v_loc.extend();
-v_loc(4) := 'Gharbia';
-dbms_output.put_line('my city  : ' || v_loc(4));
-
-
+   v_loc.extend();
+   v_loc(4) := 'Gharbia';
+   dbms_output.put_line('my city  : ' || v_loc(4));
 end;
 /
 
-select * from departments;
+select *
+  from departments;
 
-declare 
-
-cursor marketing_emps is select * from employees where department_id = 30;
+declare
+   cursor marketing_emps is
+   select *
+     from employees
+    where department_id = 30;
 
 begin
 
@@ -133,45 +137,64 @@ begin
 
 -- open marketing_emps;
 
-for emp in marketing_emps loop
-dbms_output.put_line('Employee Name: ' || emp.first_name || ' ' || emp  
-.last_name);
-end loop;
+   for emp in marketing_emps loop
+      dbms_output.put_line('Employee Name: '
+                           || emp.first_name
+                           || ' ' || emp.last_name);
+   end loop;
 
 -- marketing_emps%rowcount;
 -- dbms_output.put_line(marketing_emps%rowcount );
 -- dbms_output.put_line(marketing_emps%isopen );
 
-if marketing_emps%isopen then
-dbms_output.put_line('Cursor is open');
+   if marketing_emps%isopen then
+      dbms_output.put_line('Cursor is open');
+   else
+      dbms_output.put_line('Cursor is closed');
+   end if;
 
-else    
-dbms_output.put_line('Cursor is closed');
-end if;
-
-
-end;
-/   
-
-
-select * from employees where department_id = 20;
-
-declare 
-
-
-cursor marketing_emps is select * from employees where department_id = 20 for update;
-
-begin 
-
-    for emp in marketing_emps loop
-        update employees set salary = salary + 100
-        where current of marketing_emps;
-
-    end loop;
 
 end;
 /
 
 
+select *
+  from employees
+ where department_id = 20;
+
+declare
+   cursor marketing_emps is
+   select *
+     from employees
+    where department_id = 20
+   for update;
+
+begin
+   for emp in marketing_emps loop
+      update employees
+         set
+         salary = salary + 100
+       where current of marketing_emps;
+
+   end loop;
+end;
+/
 
 
+commit;
+
+
+declare
+   v_emp_name employees.first_name%type;
+begin
+   select first_name
+     into v_emp_name
+     from employees
+    where employee_id = 1;
+
+   dbms_output.put_line('Employee Name: ' || v_emp_name);
+exception
+   when no_data_found then
+      dbms_output.put_line('No employee found with the given ID');
+end;
+/
